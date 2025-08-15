6e6420c19ac47505f8083bdd43e1c55a8f18ab86
ArtoriasCode <artorias.code@gmail.com>
2025-08-15 22:41:34 +0300

    Styles updated

--- examples/weather_app/frontend/src/App.vue
+++ examples/weather_app/frontend/src/App.vue
@@ -20,12 +20,34 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  place-items: center;
+  padding: 20px;
+  min-height: 100vh;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  margin: 0;
+}
+
+html {
+  background-color: #eaeaea;
+}
+
+* {
+  font-family: "Montserrat", sans-serif;
 }
 </style>
--- examples/weather_app/frontend/src/components/WeatherBlock.vue
+++ examples/weather_app/frontend/src/components/WeatherBlock.vue
@@ -66,39 +66,7 @@ async function fetchWeather() {
 }
 </script>
 
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-
-html, body {
-  height: 100%;
-}
-
-html, body, #app {
-  margin: 0;
-  padding: 0;
-}
-
-p, h1, h2, h3, h4, h5, h6 {
-  margin: 0;
-}
-
-html {
-  background-color: #eaeaea;
-}
-
-* {
-  font-family: "Montserrat", sans-serif;
-}
-
-#app {
-  display: grid;
-  place-items: center;
-  padding: 20px;
-  margin-top: 0 !important;
-  min-height: 100vh;
-  box-sizing: border-box;
-}
-
+<style lang="scss" scoped>
 .content {
   display: flex;
   justify-content: center;
@@ -249,18 +217,22 @@ html {
   }
 }
 
+@media (max-width: 1024px) {
+  .weather {
+    .forecast {
+      .days {
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+    }
+  }
+}
+
 @media (max-width: 768px) {
   .weather {
     .current {
       .temp {
         font-size: 150px;
-      }
-    }
-
-    .forecast {
-      .days {
-        flex-wrap: wrap;
-        justify-content: center;
       }
     }
   }
